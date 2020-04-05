260d3065678cce6155b197315885f31f5d7de6bb
Jabran Rafique <hello@jabran.me>
2020-04-05 02:14:20 +0100

    update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react";
-import { useLocation, Link } from "react-router-dom";
-import cookie from "js-cookie";
+import React, { useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import cookie from 'js-cookie';
 
-import style from "./style.module.scss";
+import style from './style.module.scss';
 
 const Layout = ({ children }) => {
   const location = useLocation();
 
-  const [hasCookiesConsent, setCookieConsent] = useState(
-    cookie.get("cookie-consent")
-  );
+  const [hasCookiesConsent, setCookieConsent] = useState(cookie.get('cookie-consent'));
 
   return (
     <div className={style.layout}>
       {!hasCookiesConsent && (
         <div className={style.cookie}>
-          This website uses cookies to deliver better user experience. By
-          continuing to use this website you agree to the use of cookies.{" "}
+          This website uses cookies to deliver better user experience. By continuing to use this website you agree to
+          the use of cookies.{' '}
           <a
             href="#"
-            onClick={ev => {
+            onClick={(ev) => {
               ev.preventDefault();
-              cookie.set("cookie-consent", "accepted");
+              cookie.set('cookie-consent', 'accepted');
               setCookieConsent(true);
             }}
           >
@@ -30,7 +28,7 @@ const Layout = ({ children }) => {
         </div>
       )}
       <header className={style.header}>
-        {location.pathname === "/" ? (
+        {location.pathname === '/' ? (
           <div className={style.branding}>
             <h1 className={style.logo}>COVIDONATION</h1>
             <div className={style.tagline}>Ways to help</div>
@@ -43,35 +41,22 @@ const Layout = ({ children }) => {
             <div className={style.tagline}>Ways to help</div>
           </div>
         )}
-        {/* <nav className={style.nav}>
-          <div>
-            WHO WhatsApp <a href="tel:+41798931892">+417 9893 1892</a>
-          </div>
-        </nav> */}
       </header>
       <main className={style.main}>{children}</main>
       <footer className={style.footer}>
         <div>&copy; 2020 - MIT License</div>
         <div>
-          Data sources are JHU, CSSE and WHO via{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.covid19api.com?utm_source=covidonation"
-          >
+          Data sources are JHU, CSSE and WHO via{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://www.covid19api.com?utm_source=covidonation">
             www.covid19api.com
           </a>
         </div>
-        <div>
-          Get in touch at{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.twitter.com/jabranr?utm_source=covidonation"
-          >
+        <div className={style['colophone']}>
+          Get in touch at{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/jabranr?utm_source=covidonation">
             Twitter
-          </a>{" "}
-          Contribute at{" "}
+          </a>
+          &nbsp;&bull;&nbsp;Contribute at{' '}
           <a
             target="_blank"
             rel="noopener noreferrer"
@@ -79,8 +64,8 @@ const Layout = ({ children }) => {
           >
             GitHub
           </a>
-          <p>&bull; Built for humanity</p>
         </div>
+        <p>&hearts; For humanity</p>
       </footer>
     </div>
   );
