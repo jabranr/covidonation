e5b3d5a97a347274c62699f52efd59b4374dabfc
Jabran Rafique <hello@jabran.me>
2020-04-05 02:04:14 +0100

    add countries

--- setup.js
+++ setup.js
@@ -1,13 +1,12 @@
-#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
 
-const fs = require("fs");
-const path = require("path");
-
-const countries = path.resolve(__dirname, "./src/assets/data/countries");
+const countries = path.resolve(__dirname, './src/assets/data/countries');
 const list = fs.readdirSync(countries);
-const cleanedList = list.map(v => v.replace(/\.json/gi, "")).join("','");
+const cleanedList = list.map((v) => v.replace(/\.json/gi, '')).join("','");
 
 fs.writeFileSync(
-  path.resolve(__dirname, "./src/assets/data/index.js"),
-  `export default ['${cleanedList}']`
+  path.resolve(__dirname, './src/assets/data/index.js'),
+  `/* Automatically generated file. DO NOT edit manually. */
+export default ['${cleanedList}']`
 );
