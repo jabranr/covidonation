50f898db10a77f54e18ac3a97d6e441f323270ec
Jabran Rafique <hello@jabran.me>
2020-04-10 10:40:14 +0100

    add minimum datset validity tests. resolves #6

---
+++ src/__tests__/minimum-dataset.test.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const countries = require('../assets/data');
+const countryFilesPath = path.resolve(__dirname, '../assets/data/countries');
+const countryFilesList = fs.readdirSync(countryFilesPath);
+
+describe('Minimum dataset', () => {
+  it('should have equal number of files', () => {
+    expect(countryFilesList.length).toEqual(countries.default.length);
+  });
+
+  countryFilesList.forEach((filename) => {
+    const fileContent = fs.readFileSync(`${countryFilesPath}/${filename}`);
+    const content = JSON.parse(fileContent);
+
+    describe(`Minimum datset for ${content.country}`, () => {
+      it('should have country name', () => {
+        expect(content).toHaveProperty('country');
+      });
+
+      it('should have country name as string', () => {
+        expect(Object.prototype.toString.call(content.country)).toEqual('[object String]');
+      });
+
+      it('should have country slug', () => {
+        expect(content).toHaveProperty('slug');
+      });
+
+      it('should have country slug as string', () => {
+        expect(Object.prototype.toString.call(content.slug)).toEqual('[object String]');
+      });
+
+      it('should have orgs', () => {
+        expect(content).toHaveProperty('orgs');
+      });
+
+      it('should have list of orgs as an array', () => {
+        expect(Object.prototype.toString.call(content.orgs)).toBe('[object Array]');
+      });
+    });
+  });
+});
