b14ff490aec9855500f2d0ee368350de66b417e4
Jabran Rafique <hello@jabran.me>
2020-04-16 23:33:44 +0100

    issue #1 generate routes for better performance

--- setup.js
+++ setup.js
@@ -13,6 +13,7 @@ const countries = path.resolve(__dirname, './src/assets/data/countries');
 const flags = path.resolve(__dirname, './public/assets/flags');
 const countryList = fs.readdirSync(countries);
 const countryData = [];
+const countryRoutes = [];
 
 countryList.forEach((c) => {
   const content = JSON.parse(fs.readFileSync(`${countries}/${c}`));
@@ -24,10 +25,33 @@ countryList.forEach((c) => {
     iso2: content.iso2Code.toLowerCase(),
     hasFlag
   });
+
+  countryRoutes.push(
+    `<CovidRoute exact path="/${content.slug}" slug="${content.slug}" render={(props) => <CountryPage slug="${content.slug}" {...props} />} />`
+  );
 });
 
+// write file with slugs for all countries
 fs.writeFileSync(
   path.resolve(__dirname, './src/assets/data/index.js'),
   `/* Automatically generated file. DO NOT edit manually. See setup.js for details. */
 export default ${JSON.stringify(countryData)};`
+);
+
+// write file with routes based on slugs for all countries
+fs.writeFileSync(
+  path.resolve(__dirname, './src/routes/country-routes.js'),
+  `/* Automatically generated file. DO NOT edit manually. */
+import React from 'react';
+
+import CountryPage from '../pages/country';
+import CovidRoute from './covid-route';
+
+const CountryRoutes = () => (
+  <>
+    ${countryRoutes.join('\n')}
+  </>
+);
+
+export default CountryRoutes;`
 );
