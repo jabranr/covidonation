64cd0903347173e6eeb72614aab10c8581860afc
Jabran Rafique <hello@jabran.me>
2020-04-18 18:00:01 +0100

    issue #42 - add signup form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useReducer } from 'react';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import axios from 'axios';
 
 import Layout from '../components/layout';
 import countries from '../assets/data';
 import CountryCard from '../components/country-card';
+import genericReducer from '../store/generic-reducer';
 import { TickIcon } from './country';
 
 import style from './style.module.scss';
-import { Link } from 'react-router-dom';
 
 const SearchIcon = ({ width = 18, height = 18, ...attrs }) => (
   <svg width={width} height={height} viewBox={`0 0 ${width * 2} ${height * 2}`} fill="currentColor" {...attrs}>
@@ -16,8 +18,48 @@ const SearchIcon = ({ width = 18, height = 18, ...attrs }) => (
 );
 
 const HomePage = () => {
+  const formRef = useRef();
   const imagesRef = useRef([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
+  const [signupForm, dispatchSignupForm] = useReducer(genericReducer, {
+    isValid: false,
+    isSubmitting: false,
+    data: {
+      u: '659f1405e2b20906f44429441',
+      id: 'f8f1d42230'
+    }
+  });
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+
+    // eslint-disable-next-line no-useless-escape
+    if (formRef.current.checkValidity() && /^([\w_\.\-\+])+\@([\w\-]+\.)+([\w]{2,10})+$/.test(signupForm.data.MERGE0)) {
+      dispatchSignupForm({ isSubmitting: true });
+
+      if (Boolean(signupForm.error)) {
+        dispatchSignupForm({ error: '' });
+      }
+
+      try {
+        await axios.post(formRef.current.getAttribute('action'), signupForm.data, {
+          withCredentials: true
+        });
+
+        dispatchSignupForm({
+          success: 'You have subscribed successfully',
+          data: { ...signupForm.data, MERGE0: '' }
+        });
+      } catch (err) {
+        console.log(err.response, err.data);
+        dispatchSignupForm({ error: 'An error occurred while trying to subscribed' });
+      } finally {
+        dispatchSignupForm({ isSubmitting: false });
+      }
+    } else {
+      dispatchSignupForm({ error: 'Please provide a valid email address' });
+    }
+  };
 
   useEffect(() => {
     if ('loading' in HTMLImageElement.prototype) {
@@ -37,6 +79,48 @@ const HomePage = () => {
           Here you can find details about different organisations, charities, individuals who are trying to help the
           vulnerables during the panedmic of Coronavirus (COVID-19).
         </p>
+
+        <div className={style['mc-form']}>
+          <p>Subscribe for updates</p>
+          <form
+            ref={formRef}
+            action="https://covidonation.us19.list-manage.com/subscribe/post"
+            method="POST"
+            noValidate
+            onSubmit={handleSubmit}
+          >
+            <input
+              className={style.input}
+              type="email"
+              autoCapitalize="off"
+              autoCorrect="off"
+              name="MERGE0"
+              id="MERGE0"
+              size="25"
+              defaultValue=""
+              onChange={(ev) => {
+                const changes = {
+                  data: {
+                    ...signupForm.data,
+                    MERGE0: ev.target.value
+                  },
+                  isValid: Boolean(ev.target.value.length) && ev.target.value.length > 7
+                };
+
+                if (signupForm.error) {
+                  changes.error = '';
+                }
+
+                dispatchSignupForm(changes);
+              }}
+            />
+            <button className={style.submit} type="submit" disabled={!signupForm.isValid || signupForm.isSubmitting}>
+              {signupForm.isSubmitting ? 'Subscribing...' : 'Subscribe'}
+            </button>
+            {signupForm.error && <div className={style.error}>{signupForm.error}</div>}
+            {signupForm.success && <div className={style.success}>{signupForm.success}</div>}
+          </form>
+        </div>
 
         <h2 className="heading">Countries</h2>
         <p className={style['lead-text']}>
@@ -68,7 +152,12 @@ const HomePage = () => {
               <p className={style['result-indicator']}>Search result</p>
               <ul className={style['search-result']}>
                 {filteredCountries.map((c, i) => (
-                  <li className={classNames(style['result-item'], { [style['even-item']]: i % 2 !== 0 })} key={c.slug}>
+                  <li
+                    className={classNames(style['result-item'], {
+                      [style['even-item']]: i % 2 !== 0
+                    })}
+                    key={c.slug}
+                  >
                     <TickIcon />
                     <Link className={style['result-link']} to={`/${c.slug}`}>
                       {c.name}
