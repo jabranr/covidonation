fe4acd3377da4b7a651856d4baa42cc5983d1148
Jabran Rafique⚡️ <jabranr@users.noreply.github.com>
2020-05-01 10:45:01 +0100

    Add data CTA (#80)
    * add link for data input
    
    * fix imports
    
    * add route
    
    * add link in header
    
    * add page
    
    * add and style icon
    
    * remove unused import

--- src/routing.js
+++ src/routing.js
@@ -6,11 +6,13 @@ import HomePage from './pages';
 import CountryRoutes from './routes/country-routes';
 import NotFound from './pages/not-found';
 import RouteWithSummary from './routes/route-with-summary';
+import AddDataPage from './pages/add-data';
 
 const Routing = () => {
   return (
     <Switch>
       <RouteWithSummary exact path="/" component={HomePage} />
+      <Route exact path="/add-data" component={AddDataPage} />
       <CountryRoutes />
       <Route exact path="*" component={NotFound} />
     </Switch>
