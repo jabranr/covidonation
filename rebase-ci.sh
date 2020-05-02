9acf2faf1d80d90687a1c9e2b34eba76cfecd79a
Jabran Rafique <hello@jabran.me>
2020-05-02 03:19:26 +0100

    update merge ci script

--- rebase-ci.sh
+++ rebase-ci.sh
@@ -1,12 +1,13 @@
 #!/bin/env bash
 
 git config --global push.default simple
+git status
 git fetch -t
 
 echo 'Rebase CI branch from master'
 
-git checkout origin/develop -B develop
-git merge origin/master --no-ff --no-edit
+git checkout develop -B develop
+git merge master --no-ff --no-edit
 git push origin develop
 
 echo 'Rebase to CI branch completed'
