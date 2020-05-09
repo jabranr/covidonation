b95ae1e3e0c35e1d085887a4348d817cfa147ef4
Jabran Rafique <hello@jabran.me>
2020-05-09 14:21:25 +0100

    add check

--- src/util.js
+++ src/util.js
@@ -1,4 +1,8 @@
 export const pushDataLayer = (data = {}) => {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push(data);
 };
