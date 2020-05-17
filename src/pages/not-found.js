138cc651cfe3031e4ba0fa0a1329c150d5f4c63f
Jabran Rafique <hello@jabran.me>
2020-05-17 12:20:10 +0100

    use title case

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 
 const NotFound = () => {
   return (
-    <Layout title="Page not found - COVIDonation">
+    <Layout title="Page not found - Covidonation">
       <div className="container">
         <h2>Not found</h2>
         <p>Sorry, the page your are looking for cannot be found.</p>
